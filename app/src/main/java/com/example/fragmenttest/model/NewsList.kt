40cf13bfed8257339e008c1e9b8fb8fde52adcc3
HUSTerCH <93471031+HUSTerCH@users.noreply.github.com>
2022-03-18 21:58:50 +0800

    init commit

--- app/src/main/java/com/example/fragmenttest/model/NewsList.kt
+++ app/src/main/java/com/example/fragmenttest/model/NewsList.kt
@@ -1,5 +1,14 @@
 package com.example.fragmenttest.model
 
 class NewsList {
-    fun 
+    fun newsList() :List<News> {
+        return listOf(
+            News(
+                newsTitle = "title1",
+                newsContent = "news1news1news1news1news1news1news1news1news1news1news1",
+                newsSort = 1
+            ),
+        )
+        
+    }
 }
